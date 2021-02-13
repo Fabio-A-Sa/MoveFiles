e545dc33183ac7827455daade6fe37500e79bfce
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-13 22:22:24 +0000

    Extensions maker

---
+++ Scripts/extensions.py
@@ -0,0 +1,20 @@
+extensions =    {
+
+                "jpg" : "enable" ,
+                'png' : "enable" ,
+                'jpeg': "enable" ,
+                'zip' : "disable" ,
+                'other' : "disable" ,
+                'other' : "disable" ,
+                'other' : "disable" ,
+                'other' : "disable" ,
+                'other' : "disable" ,
+                'other' : "disable" ,
+                'other' : "disable" ,
+                'other' : "disable" ,
+                'other' : "disable" ,
+
+                }
+
+def enable_extensions ():
+    return extensions
