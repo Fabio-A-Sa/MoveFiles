b530ab4fcce7de50fadaff3dbe9b74e62ffbb616
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-13 20:53:32 +0000

    Without class

--- Scripts/Organizer.py
+++ Scripts/Organizer.py
@@ -1,47 +1,46 @@
-""" 
+# Imports
 
-"""
 import os
 import shutil
 from datetime import datetime
 from PIL import Image
 
 
-class PhotoOrganizer:
+DATETIME_EXIF_INFO_ID = 36867
+extensions = ['jpg', 'jpeg', 'png']
 
-    DATETIME_EXIF_INFO_ID = 36867
-    extensions = ['jpg', 'jpeg', 'png']
+def folder_path_from_photo_date(file):
+    date = photo_shooting_date(file)
+    return date.strftime('%Y') + '/' + date.strftime('%Y-%m-%d')
 
-    def folder_path_from_photo_date(self, file):
-        date = self.photo_shooting_date(file)
-        return date.strftime('%Y') + '/' + date.strftime('%Y-%m-%d')
+def photo_shooting_date(file):
+    date = None
+    photo = Image.open(file)
+    if hasattr(photo, '_getexif'):
+        info = photo._getexif()
+        print(info)
+        if info:
+            if DATETIME_EXIF_INFO_ID in info:
+                date = info[DATETIME_EXIF_INFO_ID]
+                date = datetime.strptime(date, '%Y:%m:%d %H:%M:%S')
+    if date is None:
+        date = datetime.fromtimestamp(os.path.getmtime(file))
+    return date
 
-    def photo_shooting_date(self, file):
-        date = None
-        photo = Image.open(file)
-        if hasattr(photo, '_getexif'):
-            info = photo._getexif()
-            if info:
-                if self.DATETIME_EXIF_INFO_ID in info:
-                    date = info[self.DATETIME_EXIF_INFO_ID]
-                    date = datetime.strptime(date, '%Y:%m:%d %H:%M:%S')
-        if date is None:
-            date = datetime.fromtimestamp(os.path.getmtime(file))
-        return date
+def move_photo(file):
+    new_folder = folder_path_from_photo_date(file)
+    if not os.path.exists(new_folder):
+        os.makedirs(new_folder)
+    shutil.move(file, new_folder + '/' + file)
 
-    def move_photo(self, file):
-        new_folder = self.folder_path_from_photo_date(file)
-        if not os.path.exists(new_folder):
-            os.makedirs(new_folder)
-        shutil.move(file, new_folder + '/' + file)
+def organize():
+    photos = [
+        filename for filename in os.listdir('.')
+        if os.path.isfile(filename) and any(filename.lower().endswith('.' + ext.lower()) for ext in extensions)
+    ]
+    for filename in photos:
+        move_photo(filename)
 
-    def organize(self):
-        photos = [
-            filename for filename in os.listdir('.')
-                if os.path.isfile(filename) and any(filename.lower().endswith('.' + ext.lower()) for ext in self.extensions)
-        ]
-        for filename in photos:
-            self.move_photo(filename)
+if __name__ == "__main___":            
+    organize()
 
-PO = PhotoOrganizer()
-PO.organize()
