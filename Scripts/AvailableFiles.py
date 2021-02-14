ea4e176b1a77f44bb37795d4d7b0b722f5622923
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-14 21:21:18 +0000

    More readme updates

--- Scripts/AvailableFiles.py
+++ Scripts/AvailableFiles.py
@@ -1,10 +1,18 @@
+# Change and add file status if you need
+
 available_files =   {
 
-                    "photos": "enable",
-                    "text" : "disable", 
-                    "web" : "disable"
+                    "photos": "enabled",      # .png, .jpg, .jpeg
+                    "text" : "disabled",      # .txt
+                    "web" : "disabled",       # .html, .css, .js
+                    "code" : "disabled",      # .html, .css, .js, .py, .cpp
                     
                     }
 
 def display():
-    return available_files
+
+    # Filter avail
+    availables = [type for type,status in available_files.items() if status == "enabled"]
+    print(available_files)
+    return availables
+
